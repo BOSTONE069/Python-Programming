06c577a812c3484e10445dfda01590b6e32c303b
BOSTONE069 <bostoneochieng69@gmail.com>
2022-06-28 11:27:56 -0400

    Python Programming

---
+++ convertingmiles.py
@@ -0,0 +1,11 @@
+#This is to ask the user for the input value of miles
+mile = input("Enter the value for miles: ")
+
+#This is for converting the string value of miles into integer
+mile = int(mile)
+
+#This is for converting the miles into kilometere
+kilometers = mile * 1.60934
+
+#This is for printing the value of kilometers using the string format
+print("{} Miles equal {:.2f} kilometers".format(mile, kilometers))
