ad0b5bf9d9d9a02812970a807308d8c9f3d26416
rhemney <bostoneochieng69@gmail.com>
2022-06-28 21:24:40 -0700

    Python Programming

--- compoundinterest.py
+++ compoundinterest.py
@@ -1,16 +1,16 @@
-#This is for requesting user input
+# This is for requesting user input
 money = input("How much to invest: ")
 interest_rate = input("Interest rate: ")
 
-#This is for converting money to float
+# This is for converting money to float
 money = float(money)
 
-#This is for converting a float and round the percentage rate by 2 digits
+# This is for converting a float and round the percentage rate by 2 digits
 interest_rate = float(interest_rate) * .01
 
-#This is to cycle through 10 years using a for loop from 0 to 9
+# This is to cycle through 10 years using a for loop from 0 to 9
 for i in range(10):
     money = money + (money * interest_rate)
 
-#This is for output of the results
+# This is for output of the results
 print("Investment after 10 years: {:.2f}".format(money))
