adb50da5838d15266cec30a2d5319d11b30e4f93
BOSTONE069 <bostoneochieng69@gmail.com>
2022-07-05 10:06:47 -0400

    CS50 Python Programming

--- hello.py
+++ hello.py
@@ -9,10 +9,8 @@ print("Hello,", name)
 print("Welcome to CS50 {}".format(name))
 print(f"Welcome to CS50 {name}")
 print("hello,", end="")
-print(name)
+print(name[-3])
 print("\"Hello\"")
 
 name2 = input("What is your second name: ").strip().title()
 print(f"Your second name: {name2}")
-
-
