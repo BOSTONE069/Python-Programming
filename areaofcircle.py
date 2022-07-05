4eefd34846cdb33489cfaf21ce1470b619d1de61
BOSTONE069 <bostoneochieng69@gmail.com>
2022-07-05 10:15:56 -0400

    Area of a circle

--- areaofcircle.py
+++ areaofcircle.py
@@ -2,12 +2,16 @@ import math
 
 PI = math.pi
 
+#This is requesting the user input
 radius = input("Enter the radius of the circle: ")
 
 radius = int(radius)
 
+#This is formula for calculating the PI
 area_of_circle = PI * radius * radius
 
-area_of_circle = round(area_of_circle)
+# This is to round the number to three decimal place
+area_of_circle = round(area_of_circle, 3)
 
+#This is for printing the output
 print(f"The area of a circle is {area_of_circle}")
