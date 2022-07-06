d35652d20df326f4409e4ee18c3c76aadb344e15
BOSTONE069 <bostoneochieng69@gmail.com>
2022-07-06 14:27:39 -0400

    This is for finding sum of two number

---
+++ sum.py
@@ -0,0 +1,11 @@
+number1 = int(input("Enter the first value: "))
+number2 = int(input("Enter the first value: "))
+
+addition = number1 + number2
+
+
+def mul(x, y):
+    return x * y
+
+
+print(f"The sum of the value is {addition}")
