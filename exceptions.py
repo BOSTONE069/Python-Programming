eefbd3059f89837b4b29ed9fa737d0239f5e0694
rhemney <bostoneochieng69@gmail.com>
2022-07-06 04:46:32 -0700

    CS50 Python programming EXCEPTIONS

--- exceptions.py
+++ exceptions.py
@@ -3,3 +3,34 @@ try:
     print(f"The number is {number}")
 except ValueError:
     print("The number you have entered is not an integer")
+
+# using the try with else
+try:
+    number = int(input("Enter the number: "))
+except ValueError:
+    print("The number you have entered is not an integer")
+else:
+    print(f"The number is {number}")
+
+# This is to loop several times
+while True:
+    try:
+        number = int(input("Enter the number: "))
+    except ValueError:
+        print("The number you have entered is not an integer")
+    else:
+        break
+
+print(f"The number is {number}")
+
+
+# This is to loop several times
+while True:
+    try:
+        number = int(input("Enter the number: "))
+    except ValueError:
+        pass
+    else:
+        break
+
+print(f"The number is {number}")
