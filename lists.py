7b45ffbebbd0a6d242c1518d6f3483496c159eb1
BOSTONE069 <bostoneochieng69@gmail.com>
2022-07-06 10:04:22 -0400

    CS50 Python Programming list

--- lists.py
+++ lists.py
@@ -29,5 +29,14 @@ print(students[0] * 2)
 # This is for changing elements in a list
 students[0] = "Melvin"
 
+# This is for sorting the list
+students.sort()
+
 for i in students:
     print("{} : {}".format(students.index(i), i))
+
+# List comprehension
+evenList = [i * 2 for i in range(10)]
+
+for i in evenList:
+    print(i)
