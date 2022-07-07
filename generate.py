16a7256ccf62cc4945bcc77017985c0f7cfd88c1
BOSTONE069 <bostoneochieng69@gmail.com>
2022-07-07 09:52:51 -0400

    CS50 Python Programming

--- generate.py
+++ generate.py
@@ -1,4 +1,10 @@
+import random
 from random import choice
 
 coin = choice(["head", "tails"])
 print(coin)
+
+cards = ["jack", "queen", "king"]
+random.shuffle(cards)
+for card in cards:
+    print(card)
