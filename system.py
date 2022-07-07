4b881372b1199bd470fd5af6a5252f95236895e3
BOSTONE069 <bostoneochieng69@gmail.com>
2022-07-07 10:39:11 -0400

    CS50 Python Programming

--- system.py
+++ system.py
@@ -1,3 +1,8 @@
 import sys
 
-print("Hello welcome to CS50 ", sys.argv[1])
+if len(sys.argv) < 2:
+    print("There is no argument provided")
+elif len(sys.argv) > 2:
+    print("Too many argument provided")
+else:
+    print("Hello welcome to CS50 ", sys.argv[1])
