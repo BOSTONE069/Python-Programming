1c97def22ec9399fe1eeb53ed890d5d34e88d630
rhemney <bostoneochieng69@gmail.com>
2022-07-12 20:52:21 -0700

    CS50 Python Programming for magical calculations

--- magiccalculations.py
+++ magiccalculations.py
@@ -2,7 +2,7 @@
 def magic_calculation(a, b, c):
     """Match bytecode provided by Holberton School."""
     if a < b:
-        return (c)
+        return c
     if c > b:
         return (a + b)
-    return (a*b - c)
+    return a * b - c
