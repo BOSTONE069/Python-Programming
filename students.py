d651a1efb3515f80048a6390a6acdd0222d90233
BOSTONE069 <bostoneochieng69@gmail.com>
2022-07-14 16:45:44 -0400

    CS50 Python Programming for file input and output.

--- students.py
+++ students.py
@@ -1,4 +1,4 @@
 with open("students.csv") as file:
     for line in sorted(file):
-        row = line.rstrip().split(",")
-        print(f"{row[0]} is in {row[1]}")
+        name, country = line.rstrip().split(",")
+        print(f"{name} is in {country}")
