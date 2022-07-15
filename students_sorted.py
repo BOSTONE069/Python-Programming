7ab75087a1c04ad52ee0b34d39287ead2f511439
BOSTONE069 <bostoneochieng69@gmail.com>
2022-07-15 09:13:52 -0400

    CS50 Python Programming file input and output

---
+++ students_sorted.py
@@ -0,0 +1,15 @@
+students = []
+
+with open("students.csv") as file:
+    for line in file:
+        name, country = line.rstrip().split(",")
+        student = {"name": name, "country": country}
+        students.append(student)
+
+
+def get_names(student):
+    return student["name"]
+
+
+for student in sorted(students, key=get_names):
+    print(f"{student['name']} is in {student['country']}")
