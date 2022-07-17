53dc938c307519d8120a0dc38a8bf99797a2977f
BOSTONE069 <bostoneochieng69@gmail.com>
2022-07-17 05:39:51 -0400

    CS50 Python Programming regular expressions for testing the validity of the email address

--- validate.py
+++ validate.py
@@ -2,7 +2,7 @@ import re
 
 email = input("What is your email address? ").strip()
 
-if re.search(".+@.+\.com", email):
+if re.search("^[^@]+@[^@].+\.com$", email):
     print("valid")
 else:
     print("invalid")
