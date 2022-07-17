0570d9a9e272690d31b36354e24f531645ad591b
BOSTONE069 <bostoneochieng69@gmail.com>
2022-07-17 05:19:16 -0400

    CS50 Python Programming regular expressions

--- validate.py
+++ validate.py
@@ -1,6 +1,8 @@
+import re
+
 email = input("What is your email address? ").strip()
 
-if "@" in email and "." in email:
+if re.search(".+@.+\.com", email):
     print("valid")
 else:
     print("invalid")
