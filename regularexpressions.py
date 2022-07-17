5a95bd09a7b74c3598b8e7dd8f8c9f26dc3af013
BOSTONE069 <bostoneochieng69@gmail.com>
2022-07-17 12:37:50 -0400

    CS50 Python Programming regular expression

--- regularexpressions.py
+++ regularexpressions.py
@@ -2,7 +2,7 @@ import re
 
 url = input("URL: ").strip()
 
-if matches := re.search("^(https?://)?(?:www\.)?twitter\.com/(.+)$", url, re.IGNORECASE):
+if matches := re.search("^(https?://)?(?:www\.)?twitter\.com/(\w+)", url, re.IGNORECASE):
     print(f"Username:", matches.group(2))
 else:
     pass
