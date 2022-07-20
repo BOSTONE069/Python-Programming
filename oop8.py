8c0b47c2dfdda8e49ed334eef3d1d5304bcb53a5
BOSTONE069 <bostoneochieng69@gmail.com>
2022-07-20 15:06:19 -0400

    CS50 Python Programming Object-Oriented Programming for class method

--- oop8.py
+++ oop8.py
@@ -1,4 +1,9 @@
 class Student:
+    """
+    The __init__ function is called when a new instance of the class is created.
+    param name: The name of the city
+    :param country: The country the city is in
+    """
     def __init__(self, name, country):
         self.name = name
         self.country = country
@@ -6,6 +11,14 @@ class Student:
     def __str__(self):
         return f"{self.name} from {self.country}"
 
+    """
+    The @classmethod decorator is used to create a 
+    method that can be called either on the class (such as C.get()) or on
+    an instance (such as C().get())
+
+    :param cls: This is the class object
+    :return: The class is being returned.
+    """
     @classmethod
     def get(cls):
         name = input("Enter the value of your name here: ")
@@ -14,6 +27,9 @@ class Student:
 
 
 def main():
+    """
+    `main()` gets a student from the database and prints it
+    """
     student = Student.get()
     print(student)
 
