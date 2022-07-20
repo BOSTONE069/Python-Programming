de0b5a73d975b2de684ed4db67e336ab8a741625
BOSTONE069 <bostoneochieng69@gmail.com>
2022-07-20 15:57:21 -0400

    CS50 Python Programming Object-Oriented Programming for class operator overload

--- oop10_overload.py
+++ oop10_overload.py
@@ -1,5 +1,13 @@
 class Vault:
     def __init__(self, galleons=0, sickles=0, knuts=0):
+        """
+        This function takes in three parameters, galleons, sickles, and knuts, and sets the values of the instance variables
+        galleons, sickles, and knuts to the values of the parameters galleons, sickles, and knuts.
+
+        :param galleons: The number of galleons in the amount of money, defaults to 0 (optional)
+        :param sickles: The number of sickles in the amount of money, defaults to 0 (optional)
+        :param knuts: The smallest unit of currency in the wizarding world, defaults to 0 (optional)
+        """
         self.galleons = galleons
         self.sickles = sickles
         self.knuts = knuts
@@ -8,6 +16,13 @@ class Vault:
         return f"{self.galleons} Galleons, {self.sickles} Sickles, {self.knuts} Knuts"
 
     def __add__(self, other):
+        """
+        It takes two objects, adds their galleons, sickles, and knuts, and returns a new Vault object with the sum of the
+        galleons, sickles, and knuts
+
+        :param other: The other Vault object that is being added to the current Vault object
+        :return: A Vault object
+        """
         galleons = self.galleons + other.galleons
         sickles = self.sickles + other.sickles
         knuts = self.knuts + other.knuts
