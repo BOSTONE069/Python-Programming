61262e60f3e254a3c026144c99bed6ae74e153b4
BOSTONE069 <bostoneochieng69@gmail.com>
2022-07-21 08:39:33 -0400

    CS50 Python Programming global variables

--- global_variable.py
+++ global_variable.py
@@ -2,6 +2,9 @@ balance = 0
 
 
 def main():
+    """
+    It prints the current balance, withdraws $100, deposits $400, and prints the new balance
+    """
     print("Balance:", balance)
     withdraw(100)
     deposit(400)
@@ -9,11 +12,21 @@ def main():
 
 
 def withdraw(n):
+    """
+    The withdraw function subtracts the value of n from the global variable balance.
+
+    :param n: the amount to withdraw
+    """
     global balance
     balance -= n
 
 
 def deposit(n):
+    """
+    The deposit function adds the amount n to the global variable balance.
+
+    :param n: the amount of money to deposit
+    """
     global balance
     balance += n
 
