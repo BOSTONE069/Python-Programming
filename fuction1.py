f8a2410b8eb25202a0d1ed58cef0a847ab5abd03
BOSTONE069 <bostoneochieng69@gmail.com>
2022-07-29 13:43:54 -0400

    CS50 Python Programming

--- fuction1.py
+++ fuction1.py
@@ -3,3 +3,22 @@ def my_function(counter=89):
 
 
 my_function()
+
+#Ask user for the name
+name = input("What is your name? ")
+
+second_name = input("What is your second name? ").strip().title()
+
+# Remove whitespace from names
+name = name.strip()
+
+name = name.capitalize()
+
+print("Hello: {}".format(name))
+print(f"Hello: {name}")
+print("Hello,", name)
+print("Hello" + name)
+
+print("Please: {}".format(second_name))
+
+
