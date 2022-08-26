3e370db8e34d537609364e226003bea7f0ddde57
BOSTONE069 <bostoneochieng69@gmail.com>
2022-08-26 15:32:10 -0400

    CS50 Python Programming regular expression

--- conditionals.py
+++ conditionals.py
@@ -1,6 +1,7 @@
 x = int(input("Enter the value of x: "))
 y = int(input("Enter the value of y: "))
 
+# Comparing the values of x and y and printing the result.
 if x < y:
     print("x is less than y")
 elif x > y:
--- twitter.py
+++ twitter.py
@@ -2,6 +2,7 @@ import re
 
 url = input("URL: ").strip()
 
+# Using a regular expression to remove the beginning of the URL.
 username = re.sub("^(https?://)?(www\.)?twitter\.com/", "", url)
 print(f"Username: {username}")
 
